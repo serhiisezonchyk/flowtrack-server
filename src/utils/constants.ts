cad97da98ee777c063c9722c1c458ce2016be351
serhiisezonchyk <sergey040901@gmail.com>
2024-06-19 21:55:58 +0300

    feat: add DnD functionality

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,8 @@
 export const COOKIE_SETTINGS = {
   REFRESH_TOKEN: {
     httpOnly: true,
-    // maxAge: 1 * 24 * 60 * 60 * 1000, //1d
-    maxAge: 1 * 60 * 1000, //1d
+    maxAge: 1 * 24 * 60 * 60 * 1000, //1d
   },
 };
-// export const ACCESS_TOKEN_EXPIRATION = 15 * 60 * 1000; //15m
-export const ACCESS_TOKEN_EXPIRATION = 1 * 30 * 1000; //15m
+export const ACCESS_TOKEN_EXPIRATION = 15 * 60 * 1000; //15m
+
