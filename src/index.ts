71dd122d318d09f4928609710afdf28e837038e6
serhiisezonchyk <sergey040901@gmail.com>
2024-06-29 17:45:59 +0300

    feat: redy for prod

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { routeNotFound } from './middleware/routeNotFound';
 import router from './routes';
 import scheduler from 'node-schedule';
 import tokenCleanUp from './utils/tokenCleanup';
-import path from "path";
+import path from 'path';
 
 dotenv.config();
 
@@ -18,19 +18,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api', router);
 
-
 //Log not found routes
-app.use(routeNotFound);
+if (process.env.NODE_ENV === 'development') app.use(routeNotFound);
 
 //BUILD
-// const __dirname = path.resolve();
-const environment = process.env.NODE_ENV;
-console.log(environment)
-if(process.env.NODE_ENV !=='development'){
-  app.use(express.static(path.join(__dirname,'/front_build')));
-  app.get('*',(req:Request,res:Response)=>{
-    res.sendFile(path.join(__dirname,"front_build","index.html"))
-  })
+if (process.env.NODE_ENV !== 'development') {
+  app.use(express.static(path.join(__dirname, '/front_build')));
+  app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, 'front_build', 'index.html'));
+  });
 }
 
 scheduler.scheduleJob('0 0 * * *', tokenCleanUp);
